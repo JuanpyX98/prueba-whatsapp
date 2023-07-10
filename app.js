cddd6d19a8b2667aefd7d665253d195b2309036d
RobotUser98 <78767767+RobotUser98@users.noreply.github.com>
2023-07-09 22:06:33 -0300

    modificacion3

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ const mytoken = process.env.MYTOKEN;
 
 app.get('/webhook', (req, res) => {
     let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"]
+    let challange = req.query["hub.challenge"]
     let token = req.query["hub.verify_token"]
     res.send("Holaa")
     
     if(mode === "subscribe" && token === mytoken){
         res
             .status(200)
-            .send(challenge);
+            .send(challange);
     }else{
         res.status(403);
     }
